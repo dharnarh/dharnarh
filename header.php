614ebd1b0506f22700235abd09a2b2e8f569c2aa
Umar Farouq Mohammed <upharouk@gmail.com>
2021-01-26 18:06:08 +0000

    header and footer

---
+++ header.php
@@ -0,0 +1,55 @@
+<?php $devMode = $_SERVER['SERVER_NAME'] === 'umar.website'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php wp_head(); ?>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Hi! I'm Umar Farouq Mohammed</title>
+
+  <!-- meta -->
+  <meta name="robots" content="index, follow" />
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="language" content="English" />
+  <meta name="Classification" content="Personal" />
+  <meta name="revisit-after" content="7 days">
+
+  <!-- favicon generator -->
+  <link rel="icon" type="image/x-icon" href="<?= get_template_directory_uri() . '/assets/favicon/favicon.ico'; ?>" />
+  <link rel="apple-touch-icon" sizes="180x180"
+    href="<?= get_template_directory_uri() . '/assets/favicon/apple-touch-icon.png'; ?>" />
+  <link rel="icon" type="image/png" sizes="32x32"
+    href="<?= get_template_directory_uri() . '/assets/favicon/favicon-32x32.png'; ?>" />
+  <link rel="icon" type="image/png" sizes="16x16"
+    href="<?= get_template_directory_uri() . '/assets/favicon/favicon-16x16.png'; ?>" />
+  <link rel="manifest" href="<?= get_template_directory_uri() . '/assets/favicon/site.webmanifest'; ?>" />
+
+  <!-- css fonts and bootstrap -->
+  <link rel="preconnect" href="https://fonts.gstatic.com" />
+  <link href="https://fonts.googleapis.com/css2?family=Jost:wght@400;700&display=swap" rel="stylesheet" />
+  <?php if (! $devMode) : ?>
+  <link rel="stylesheet" href="<?= get_template_directory_uri() . '/assets/css/bootstrap.css'; ?>" />
+  <?php else : ?>
+  <link rel="stylesheet" href="<?= get_template_directory_uri() . '/assets/css/bootstrap.min.css'; ?>" />
+  <?php endif; ?>
+
+  <!-- ld json -->
+  <script type="application/ld+json">
+  {
+    "@context": "http://schema.org",
+    "@type": "Person",
+    "email": "mailto:hey@umar.website",
+    "image": "https://umar.website/wp-content/themes/umar/assets/images/mine.jpg",
+    "jobTitle": "Self-employed",
+    "name": "Umar Farouq Mohammed",
+    "gender": "mail",
+    "url": "https://umar.website",
+    "description": "Software Developer, UI Designer, Instructor"
+  }
+  </script>
+</head>
+
+<body class="bg-white">
+  <?php get_template_part( 'template-parts/header/nav' ); ?>

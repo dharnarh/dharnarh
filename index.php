e3d396026ebff8938fc38591aa4e40b6b385825f
Umar Farouq Mohammed <upharouk@gmail.com>
2021-01-26 18:06:18 +0000

    index

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php get_header(); ?>
+
+<div class="container-fluid bg-light p-0">
+  <div class="container banner d-flex align-items-center justify-content-center">
+    <div class="d-block text-center">
+      <h1 class="display-4 fw-bold"><span class="text-primary">My Articles</span></h1>
+      <h4 class="h4 fw-normal">All articles are original and written by me.</h4>
+    </div>
+  </div>
+</div>
+
+<div class="my-5 py-3"></div>
+
+<div class="container p-0">
+  <div class="row">
+    <div class="col-10 col-md-10 col-lg-7 mx-auto">
+      <div class="d-block">
+        <a href="#" class="text-decoration-none">
+          <h2 class="h2 fw-bold">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h2>
+        </a>
+        <p class="mt-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores deleniti, quod atque aut
+          suscipit autem
+          error nemo provident doloribus numquam quae cum hic consequatur? Magnam exercitationem deserunt fuga est quam!
+        </p>
+        <p class="small">Date: 20 January, 2021 | Posted by Me</p>
+      </div>
+      <hr class="my-5">
+    </div>
+
+    <div class="col-10 col-md-10 col-lg-7 mx-auto">
+      <div class="d-block text-center">
+        <h2 class="h2 fw-bold">No articles yet.</h2>
+        <p class="mt-3">Check back in a few days.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php get_footer(); ?>

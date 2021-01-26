f357b63490c70d76c9eb74990fe21a647765899d
Umar Farouq Mohammed <upharouk@gmail.com>
2021-01-26 23:19:00 +0000

    page template

---
+++ template-parts/content/content-page.php
@@ -0,0 +1,7 @@
+<div class="col-10 col-md-10 col-lg-7 mx-auto">
+  <div class="d-block">
+    <p class="small">Date: <?php the_date(); ?></p>
+    <hr class="mb-5">
+    <?php the_content(); ?>
+  </div>
+</div>

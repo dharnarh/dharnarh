5f51e0ad763fc7ecd59544ff4dc21190dd94d880
Umar Farouq Mohammed <upharouk@gmail.com>
2021-04-03 21:37:32 +0000

    update navigation

--- template-parts/header/nav.php
+++ template-parts/header/nav.php
@@ -15,17 +15,21 @@
       <div class="col-12 col-sm">
         <ul class="nav py-2 py-lg-0 justify-content-center justify-content-sm-end">
           <li class="nav-item">
-            <a class="nav-link text-dark" href="/about">About Me</a>
+            <a class="nav-link px-2 px-md-3 text-dark" href="/about">About Me</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link text-dark"
+            <a class="nav-link px-2 px-md-3 text-dark"
               href="<?php echo get_permalink( get_option( 'page_for_posts' ) ) ?>">Articles</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link text-dark" href="/course">Courses</a>
+            <a class="nav-link px-2 px-md-3 text-dark" href="/course">Courses</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link btn-primary" href="https://thedevbuild.com" target="_blank" rel="noreferrer">Hire
+          <li class="nav-item d-block d-sm-none">
+            <a class="nav-link px-2 px-md-3 text-dark" href="https://thedevbuild.com" target="_blank"
+              rel="noreferrer">Hire Me</a>
+          </li>
+          <li class="nav-item d-none d-sm-block">
+            <a class="nav-link btn-outline-primary" href="https://thedevbuild.com" target="_blank" rel="noreferrer">Hire
               Me</a>
           </li>
         </ul>
